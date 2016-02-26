f8a8ce8981559595fb08719c2ae251e2e6542c6f
alexgermann <alexgermann@github.com>
2016-02-25 18:40:35 -0700

    Start of the back-end work with Auston.

--- src/GameTile.java
+++ src/GameTile.java
@@ -1,29 +1,91 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class GameTile implements Drawable {
+public class GameTile implements Drawable 
+{
+  
+  
   
   public static final int SIZE = 100;
+    
+    // We should probably inherit from a some kind of base class, since
+    // this behavior is the same as the Board's.
+    // If we do, it should probably replace the Drawable interface.
+    private int locX;
+    private int locY;
+    private int correctArrayPos;
+    //private int currentArrayPos;
+    
+    
+    public void setScreenLoc(int x, int y) {
+      locX = x;
+      locY = y;
+    }
+    
+    // This should eventually be drawing the parts of the image.
+    @Override
+    public void draw(Graphics g) {
+      g.setColor(Color.ORANGE);
+      g.fillRect(locX, locY, SIZE, SIZE);
+    }
+
+
   
-  // We should probably inherit from a some kind of base class, since
-  // this behavior is the same as the Board's.
-  // If we do, it should probably replace the Drawable interface.
-  private int locX;
-  private int locY;
-  
-  public void setScreenLoc(int x, int y) {
-    locX = x;
-    locY = y;
+  //Initializing GameTile. currentArrayPos was only needed if we are going to
+  //manipulate the array past initialization.
+  public GameTile(int arrayPos)
+  {
+    //currentArrayPos = arrayPos;
+    correctArrayPos = arrayPos;
   }
   
-  // This should eventually be drawing the parts of the image.
-  @Override
-  public void draw(Graphics g) {
-    g.setColor(Color.ORANGE);
-    g.fillRect(locX, locY, SIZE, SIZE);
+  
+  public int getScreenXLoc()
+  {
+    return locX;
+  }
+  
+  public int getScreenYLoc()
+  {
+    return locY;
   }
 
+  
+  //Both of these are methods that would be used if we were manipulating the array
+  //in some way (e.g. if it were used for the grid or the side array)
+  /*
+  public int getArrayPos()
+  {
+    return currentArrayPos;
+  }*/
+  
+  /*
+  public void setArrayPos(int pos)
+  {
+    currentArrayPos = pos;
+  }*/
+  
+  
+  
+  //Checks to see if the tile is in the correct position on the grid by comparing the x and y positions 
+  //to the position for the correct spot on the grid.
+  public boolean correctPosition()
+  {
+    if ((locX == 250+SIZE*(correctArrayPos%4)) && (locY == 300+SIZE*(correctArrayPos/4)))
+    {
+      return true;
+    }
+    return false;
+    
+    
+    //Originally thought we were implementing the grid as an array in which case this would
+    //be the way to do this method, corrected for pixels above.
+    /*
+    if (currentArrayPos == correctArrayPos)
+    {
+      return true;
+    }
+    return false;*/
+  }
+  
 }
-
-
-//THIS IS A TEST
